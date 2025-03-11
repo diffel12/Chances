6f2112ef882c3f70614446017fecdec139b1f395
diffel12 <105934045+diffel12@users.noreply.github.com>
2025-03-11 09:39:51 -0700

    Update Roll System.js

--- Chances/Scripts/Roll System.js
+++ Chances/Scripts/Roll System.js
@@ -31,7 +31,7 @@ function capStone(Rarities, cap) {
    return Rarities
 }
 
-const raritylists = [[0.11,0.24,0.36,0.48,0.05,0.612,0.07,0.816,0.09, 0.01], 
+const raritylists = [[0.11,0.24,0.36,0.48,0.05,0.612,0.07,0.816,0.09, 0], 
                      [0.1, 0.20, 0.15, 0.12, 0.05, 0.13, 0.07, 0.08, 0.01, 0], 
                      [0.12, 0.25, 0.18, 0.09, 0.06, 0.14, 0.11, 0.07, 0.08, 0], 
                      [0.14, 0.26, 0.18, 0.15, 0.02, 0.19, 0.03, 0.12, 0.02, 0], 
@@ -52,6 +52,7 @@ const raritylists = [[0.11,0.24,0.36,0.48,0.05,0.612,0.07,0.816,0.09, 0.01],
                      [0.13, 0.24, 0.16, 0.08, 0.01, 0.18, 0.05, 0.11, 0.04, 0], 
                      [0.11, 0.25, 0.19, 0.09, 0.02, 0.15, 0.07, 0.10, 0.02, 0], 
                      [0.09, 0.22, 0.14, 0.13, 0.02, 0.17, 0.04, 0.12, 0.05, 0]];
+
 var decToPerc = 0;
 var luckbooster = 1;
 var cooldown = false;
@@ -59,6 +60,19 @@ var HAKARIUSED = false;
 var speedboost = false;
 var adminluck = 0
 var xtraluck = 0;
+var tsb = false;
+
+function instantRoll() {
+   if (tsb == false) {
+      console.log("Instant-roll activated");
+      tsb = true;
+
+   } else if (tsb == true) {
+      console.log("Instant-roll deactivated");
+      tsb = false;
+
+   }
+}
 
 function addLuck(perc) {
    luckbooster += perc/100
@@ -71,6 +85,8 @@ function getRandomList(lists) {
    const randomIndex = Math.floor(Math.random() * lists.length); 
    return lists[randomIndex];
 }
+
+
 
 var chosenrarities = getRandomList(raritylists);
 
@@ -87,7 +103,7 @@ function setTable(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10=0) {
 
 function item(r1, r2, r3, r4, r5) {
    if (cooldown == false) {
-   if (speedboost == false) {
+   if (speedboost == false && tsb == false) {
       var totalval = 1;
       var boostedluck = capStone(applyLuckBoost(chosenrarities, luckbooster), 0.5);
       cooldown = true;
@@ -245,7 +261,7 @@ function item(r1, r2, r3, r4, r5) {
          }
          document.getElementById('button').innerHTML = "Cooldown: 0";
       }, 5000);
-   } else {
+   } else if (speedboost == true && tsb == false) {
    var totalval = 1;
       cooldown = true;
       var boostedluck = capStone(applyLuckBoost(chosenrarities, luckbooster), 0.5);
@@ -319,7 +335,7 @@ function item(r1, r2, r3, r4, r5) {
          }
          totalval *= boostedluck[r3 - 1] * 100;
          document.getElementById('totalrarity').innerHTML = r3+"% Chance";
-         decToPerc = boostedluck[value - 1] * 100;
+         decToPerc = boostedluck[r3 - 1] * 100;
          document.getElementById(r3+'b').innerHTML = r3+"'s rarity: "+decToPerc+"%";
          totalval /= 100 
          if (r3 == 5 || r3 == 7 || r3 == 9) {
@@ -402,6 +418,101 @@ function item(r1, r2, r3, r4, r5) {
          }
          document.getElementById('button').innerHTML = "Cooldown: 0";
       }, 2500);
+   } else if (tsb == true) {
+      var totalval = 1;
+      var boostedluck = capStone(applyLuckBoost(chosenrarities, luckbooster), 0.5);
+      cooldown = true;
+      document.getElementById('1').outerHTML = "<a class='num' id='1'>0</a>";
+      document.getElementById('2').outerHTML = "<a class='num' id='2'>0</a>";
+      document.getElementById('3').outerHTML = "<a class='num' id='3'>0</a>";
+      document.getElementById('4').outerHTML = "<a class='num' id='4'>0</a>";
+      document.getElementById('5').outerHTML = "<a class='num' id='5'>0</a>";
+      document.getElementById('totalrarity').innerHTML = "Rolling!";
+
+      if (HAKARIUSED == false) {
+         var same = r1
+         if (r1 == same && r2 == same && r3 == same && r4 == same && r5== same) {
+            luckbooster += same/10;
+            document.getElementById('1').outerHTML = "<a class='ledgendnum' id='1'>"+same+"</a>";
+            document.getElementById('2').outerHTML = "<a class='ledgendnum' id='2'>"+same+"</a>";
+            document.getElementById('3').outerHTML = "<a class='ledgendnum' id='3'>"+same+"</a>";
+            document.getElementById('4').outerHTML = "<a class='ledgendnum' id='4'>"+same+"</a>";
+            document.getElementById('5').outerHTML = "<a class='ledgendnum' id='5'>"+same+"</a>";
+            document.getElementById('beastiary').innerHTML = "<a class='invis'>Rarities of the numbers ----------------</a><br><a class='beast' id='1b'>1's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='2b'>2's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='3b'>3's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='4b'>4's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='5b'>5's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='6b'>6's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='7b'>7's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='8b'>8's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a><br><a class='beast' id='9b'>9's rarity: ??%</a><br><a class='invis'>---------------------------------------------</a>";
+            HAKARIUSED = true;
+            document.getElementById('boosteritem2').outerHTML = "<div class='boosteritem' id='boosteritem2'><h2>Hakari's luck "+same+""+same+""+same+"</h2> Luck Boost: "+(same*10)+"%</div>";
+         }
+      }
+
+      if (r1 == 10) {
+         document.getElementById('1').innerHTML = "0";
+      } else {
+         document.getElementById('1').innerHTML = r1;
+      }
+      totalval *= boostedluck[r1 - 1] * 100;
+      document.getElementById('totalrarity').innerHTML = totalval+"% Chance";
+      decToPerc = boostedluck[r1 - 1] * 100;
+      document.getElementById(r1+'b').innerHTML = r1+"'s rarity: "+decToPerc+"%";
+      totalval /= 100
+      if (r1 == 5 || r1 == 7 || r1 == 9) {
+         document.getElementById('1').outerHTML = "<a class='rarenum' id='1'>"+r1+"</a>";
+      }
+
+      if (r2 == 10) {
+         document.getElementById('2').innerHTML = "0";
+      } else {
+         document.getElementById('2').innerHTML = r2;
+      }
+      totalval *= boostedluck[r2 - 1] * 100;
+      document.getElementById('totalrarity').innerHTML = totalval+"% Chance";
+      decToPerc = boostedluck[r2 - 1] * 100;
+      document.getElementById(r2+'b').innerHTML = r2+"'s rarity: "+decToPerc+"%";
+      totalval /= 100
+      if (r2 == 5 || r2 == 7 || r2 == 9) {
+         document.getElementById('2').outerHTML = "<a class='rarenum' id='2'>"+r2+"</a>";
+      }
+
+      if (r3 == 10) {
+         document.getElementById('3').innerHTML = "0";
+      } else {
+         document.getElementById('3').innerHTML = r3;
+      }
+      totalval *= boostedluck[r3 - 1] * 100;
+      document.getElementById('totalrarity').innerHTML = totalval+"% Chance";
+      decToPerc = boostedluck[r3 - 1] * 100;
+      document.getElementById(r3+'b').innerHTML = r3+"'s rarity: "+decToPerc+"%";
+      totalval /= 100
+      if (r3 == 5 || r3 == 7 || r3 == 9) {
+         document.getElementById('3').outerHTML = "<a class='rarenum' id='3'>"+r3+"</a>";
+      }
+
+      if (r4 == 10) {
+         document.getElementById('4').innerHTML = "0";
+      } else {
+         document.getElementById('4').innerHTML = r4;
+      }
+      totalval *= boostedluck[r4 - 1] * 100;
+      document.getElementById('totalrarity').innerHTML = totalval+"% Chance";
+      decToPerc = boostedluck[r4 - 1] * 100;
+      document.getElementById(r4+'b').innerHTML = r4+"'s rarity: "+decToPerc+"%";
+      totalval /= 100
+      if (r4 == 5 || r4 == 7 || r4 == 9) {
+         document.getElementById('4').outerHTML = "<a class='rarenum' id='4'>"+r4+"</a>";
+      }
+
+      if (r5 == 10) {
+         document.getElementById('5').innerHTML = "0";
+      } else {
+         document.getElementById('5').innerHTML = r5;
+      }
+      totalval *= boostedluck[r5 - 1] * 100;
+      document.getElementById('totalrarity').innerHTML = totalval+"% Chance";
+      decToPerc = boostedluck[r5 - 1] * 100;
+      document.getElementById(r5+'b').innerHTML = r5+"'s rarity: "+decToPerc+"%";
+      totalval /= 100
+      if (r5 == 5 || r5 == 7 || r5 == 9) {
+         document.getElementById('5').outerHTML = "<a class='rarenum' id='5'>"+r5+"</a>";
+      }
    }
    }
 }
